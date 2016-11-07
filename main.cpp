8bb1f216ccc1738404fdb78192a9fdccde6bf059
B-Elgy <brenden.elgarten@googlemail.com>
2016-11-06 21:08:04 -0500

    Created Main.cpp
    Added Setup and Window Creation

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*Setup*/
+//Include Statements
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+//Main Function
+int main(){
+    sf::RenderWindow window(sf::VideoMode(800, 600), "C++ Graphics Test");
+    
+}
